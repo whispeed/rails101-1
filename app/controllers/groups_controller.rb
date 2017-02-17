6541c576995deb795cab2bfa6e277b77e86e45da
whispeed <whispeed@163.com>
2017-02-18 00:13:30 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -42,6 +42,19 @@ class GroupsController < ApplicationController
     redirect_to groups_path
   end
 
+  def join
+    @group = Group.find(params[:id])
+
+     if !current_user.is_member_of?(@group)
+       current_user.join!(@group)
+       flash[:notice] = "加入本讨论版成功！"
+     else
+       flash[:warning] = "你已经是本讨论版成员了！"
+     end
+
+     redirect_to group_path(@group)
+  end
+
   def quit
     @group = Group.find(params[:id])
 
@@ -54,7 +67,7 @@ class GroupsController < ApplicationController
 
     redirect_to group_path(@group)
   end
-  
+
   private
 
   def find_group_and_check_permission
